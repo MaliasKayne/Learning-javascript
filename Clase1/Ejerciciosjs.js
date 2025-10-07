8b135ca3e1c8df75aadb051d37c6709e18ea32d1
Jossie Diaz <diazjossie2@gmail.com>
2025-10-06 21:04:39 -0500

    cambios clase 2

--- Clase1/Ejerciciosjs.js
+++ Clase1/Ejerciciosjs.js
@@ -1,3 +1,10 @@
-console.log("Hola mundo");
-let cajadejossie="Yo y los babosos";
+console.log('Hola mundo');
+let cajadejossie='Yo y los babosos';
 console.log(cajadejossie);
+let frase = 'Hola soy Jossie';
+console.log(frase.length);
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.substring(9, 15));
+
+//cadena
