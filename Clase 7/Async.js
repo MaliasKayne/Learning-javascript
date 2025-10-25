f4711f3971b8f25f724d15412d50bb3d3d95103f
Jossie Diaz <diazjossie2@gmail.com>
2025-10-24 22:40:28 -0500

    Terminando Async, fetch y GET, recordando https

--- Clase 7/Async.js
+++ Clase 7/Async.js
@@ -25,12 +25,66 @@
 //         console.log(errorMessage);
 //     });
 
-function fetchData() {
-    fetch("https://rickandmortyapi.com/api/character")
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error));
+// function fetchData() {
+//     fetch("https://rickandmortyapi.com/api/character")
+//         .then((response) => response.json())
+//         .then((data) => console.log(data))
+//         .catch((error) => console.log(error));
+// }
+
+// fetchData();
+
+// async function fetchData() {
+//     try {
+//         let response = await fetch("https://rickandmortyapi.com/api/character");
+//         let data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log('Error:', error);
+//     }
+// }
+
+// fetchData();
+
+// const urls = [
+//   "https://rickandmortyapi.com/api/character",
+//   "https://rickandmortyapi.com/api/location",
+//   "https://rickandmortyapi.com/api/episode",
+// ];
+
+// async function fetchNewData() {
+//     try {
+//         for await (const url of urls) {
+//             let response = await fetch(url);
+//             let data = await response.json();
+//             console.log(data);
+//         }
+//     } catch (error) {
+//         console.log('Error:', error);
+//     }
+// }
+
+// fetchNewData();
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+function sendHTTPRequest(method, url, data) {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => {
+        return response.json();
+    });
 }
 
-fetchData();
+async function fetchPosts() {
+    const responseData = await sendHTTPRequest("GET", "https://jsonplaceholder.typicode.com/posts");
+    console.log(responseData);
+}
 
+fetchPosts();
